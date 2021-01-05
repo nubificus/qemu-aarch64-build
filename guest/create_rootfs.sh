379d402cf7cc6becf147f93f35210c924d4d0cae
Konstantinos Papazafeiropoulos <papazof@gmail.com>
2021-01-05 16:58:49 +0200

    Reduce guest kernel/rootfs size

--- guest/create_rootfs.sh
+++ guest/create_rootfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dd if=/dev/zero of=rootfs.img bs=1M count=0 seek=2048
+dd if=/dev/zero of=rootfs.img bs=1M count=0 seek=512
 mkfs.ext4 rootfs.img
 mkdir -p mnt
 sudo mount rootfs.img mnt
